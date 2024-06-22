da91fde20aeda41289012d1f342ccbae6954b90a
hugohugo130 <123244235+hugohugo130@users.noreply.github.com>
2024-06-22 22:39:50 +0800

    Update getxiaozilivelink.py

--- getxiaozilivelink.py
+++ getxiaozilivelink.py
@@ -1,11 +1,6 @@
 def getlink():
-    from github import Github as g
-    link = "hugohugo130/hugosurvival"
-    filename = "xiaozilivelink.txt"
-    git = g("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")
-    git = git.get_repo(link)
-    git = git.get_contents(filename)
-    return git.decoded_content.decode()
+    from requests import get
+    return get("https://pastebin.com/raw/VFbHJDKv").content.decode()
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     input(getlink())
